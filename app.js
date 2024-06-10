01063a732dde0fa01b3adb28458151c082ebe7c1
sreerag-13 <sreeragsaji41@gmail.com>
2024-06-10 15:37:13 +0530

    backend complete

---
+++ app.js
@@ -0,0 +1,19 @@
+const express= require("express")
+const mongoose=require("mongoose")
+const cors=require("cors")
+
+const app=express()
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.send("hello")
+})
+
+
+app.post("/contact",(req,res)=>{
+    res.send("welcome")
+})
+
+app.listen(8080,()=>{
+    console.log("server start")
+})
